e5ed9ad459795bdcac1399c6050ba27edc0ff6ac
black-software100 <jt158161@gmail.com>
2023-08-11 21:44:06 -0500

    se organiza la pagina de la fundacion

--- js/main.js
+++ js/main.js
@@ -3,72 +3,72 @@ gsap.from(".mv", { duration: 2, fadein: true, z: 1250});
 gsap.registerPlugin(ScrollTrigger) 
 
 
-  gsap.from(".mv", {
-    scrollTrigger: {
-        trigger:".img",
+  // // gsap.from(".mv", {
+  // //   scrollTrigger: {
+  // //       trigger:".img",
 
-        end:" top ",  
-        start:"top 100px",
-        scrub:1
-    }, // start the animation when ".box" enters the viewport (once) ,
-    ease:"none",
-    x: -2090,
-    duration:90
+  // //       end:" top ",  
+  // //       start:"top 100px",
+  // //       scrub:1
+  //   //}, // start the animation when ".box" enters the viewport (once) ,
+  //   // ease:"none",
+  //   // x: -2090,
+  //   // duration:90
     
-  });
+  // });
 
-  gsap.to(".cartas", {
-    scrollTrigger: {
-        trigger:".img",
-        end:" top ",  
-        start:"top 100px",
-        toggleClass:"carta",
-        scrub:1
-    }, // start the animation when ".box" enters the viewport (once) ,
-    ease:"none",
-    //boxshadow:  "5px 5px 20px rgba(0,0,0,0.4)",
+  // gsap.to(".cartas", {
+  //   scrollTrigger: {
+  //       trigger:".img",
+  //       end:" top ",  
+  //       start:"top 100px",
+  //       toggleClass:"carta",
+  //       scrub:1
+  //   }, // start the animation when ".box" enters the viewport (once) ,
+  //   ease:"none",
+  //   //boxshadow:  "5px 5px 20px rgba(0,0,0,0.4)",
 
-    background: "#d1b891",
-    color:"black",
-    border:"black",
-    duration:1,
+  //   background: "#d1b891",
+  //   color:"black",
+  //   border:"black",
+  //   duration:1,
     
-  });
+  // });
 
-  gsap.from(".pmv", {
-    scrollTrigger: {
-        trigger:".img",
+  // gsap.from(".pmv", {
+  //   scrollTrigger: {
+  //       trigger:".img",
 
-        end:" top ",  
-        start:"top 100px",
+  //       end:" top ",  
+  //       start:"top 100px",
         
-        scrub:1
-    }, // start the animation when ".box" enters the viewport (once) ,
-    ease:"none",
-    color:"transparent",
-    duration:90
-  });
+  //       scrub:1
+  //   }, // start the animation when ".box" enters the viewport (once) ,
+  //   ease:"none",
+  //   color:"transparent",
+  //   duration:90
+  // });
   
-  gsap.to(".mb", {
-    scrollTrigger: {
-        trigger:".cartas",
-        start:"top center",
-        end:" top 100px",
-        scrub:1
-    }, // start the animation when ".box" enters the viewport (once)
-    background: "#d1b891" ,
-    ease:"none",
-    duration:90,
-  });
+  // gsap.to(".mb", {
+  //   scrollTrigger: {
+  //       trigger:".cartas",
+  //       start:"top center",
+  //       end:" top 100px",
+  //       scrub:1
+  //   }, // start the animation when ".box" enters the viewport (once)
+  //   background: "#d1b891" ,
+  //   ease:"none",
+  //   duration:90,
+  // });
 
-  gsap.to(".cat", {
-    scrollTrigger: {
-        trigger:".cartas",
-        start:"top center",
-        end:" top 100px",
-        scrub:1
-    }, // start the animation when ".box" enters the viewport (once)
-    background:"#fff",
-    ease:"none",
-    duration:1,
-  });
+  // gsap.to(".cat", {
+  //   scrollTrigger: {
+  //       trigger:".cartas",
+  //       start:"top center",
+  //       end:" top 100px",
+  //       scrub:1
+  //   }, // start the animation when ".box" enters the viewport (once)
+  //   background:"#fff",
+  //   ease:"none",
+  //   duration:1,
+  // });
